af3bd6aee86e0d903dea715becb95a26095ed8ec
Sergio <38168163+FundacionBlockchainChile@users.noreply.github.com>
2021-09-03 09:45:19 -0400

    Lazy Load added to images

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Carousel, Card } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
 
 const Slider = ({ listOfImages }) => {
   const renderListOfImages = () => {
     return listOfImages.map((image, key) => (
       <Carousel.Item key={key}>
         <Card className="mb">
-          <Card.Img variant="top" src={image} />
+          <LazyLoadImage alt={image.alt} height={image.height} src={image} />
         </Card>
       </Carousel.Item>
     ))
